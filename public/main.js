e1e19d24026a6bbc8cedc2dbbb42af8fd1f6c7f1
Oleksandr Kozlenko <alexander.kozlenko@live.com>
2024-11-01 05:15:55 -0400

    1.0.0-preview

---
+++ public/main.js
@@ -0,0 +1,14 @@
+export default {
+  iconLinks: [
+    {
+      icon: 'box',
+      href: 'https://nuget.org/packages/cotopaxi',
+      title: 'NuGet'
+    },
+    {
+      icon: 'github',
+      href: 'https://github.com/alexanderkozlenko/cotopaxi',
+      title: 'GitHub'
+    }
+  ]
+}
